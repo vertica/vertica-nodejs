ecda9120cfd3daf712a1d21addd665399d687500
jonathanl-bq <72158117+jonathanl-bq@users.noreply.github.com>
2022-09-09 10:56:40 -0700

    Clean up client and connection parameters (#92)
    * Remove some parameters from client.js
    
    * Remove accidental comment
    
    * Remove replication and commented out TLS properties
    
    * Remove client_label
    
    * Try to fix failing client label tests
    
    * Remove binary
    
    * Fix merge conflict I missed

--- packages/vertica-nodejs/lib/client.js
+++ packages/vertica-nodejs/lib/client.js
@@ -46,9 +46,6 @@ class Client extends EventEmitter {
       value: this.connectionParameters.password,
     })
 
-    this.replication = this.connectionParameters.replication
-
-    this.client_label = this.connectionParameters.client_label;
     this.protocol_version = this.connectionParameters.protocol_version;
 
     var c = config || {}
@@ -67,21 +64,20 @@ class Client extends EventEmitter {
         stream: c.stream,
         tls_mode: this.connectionParameters.tls_mode,
         tls_trusted_certs: this.connectionParameters.tls_trusted_certs,
-        //tls_client_key: this.connectionParameters.tls_client_key,
-        //tls_client_cert: this.connectionParameters.tls_client_cert,
         keepAlive: c.keepAlive || false,
         keepAliveInitialDelayMillis: c.keepAliveInitialDelayMillis || 0,
         encoding: this.connectionParameters.client_encoding || 'utf8',
         client_label: this.connectionParameters.client_label,
       })
     this.queryQueue = []
-    this.binary = c.binary || defaults.binary
     this.processID = null
     this.secretKey = null
     this.tls_mode = this.connectionParameters.tls_mode || 'disable'
-    //this.tls_client_key = this.connectionParameters.tls_client_key
-    //this.tls_client_cert = this.connectionParameters.tls_client_cert
     this.tls_trusted_certs = this.connectionParameters.tls_trusted_certs
+
+    delete this.connectionParameters.tls_mode
+    delete this.connectionParameters.tls_trusted_certs
+
     this._connectionTimeoutMillis = c.connectionTimeoutMillis || 0
   }
 
@@ -656,7 +652,8 @@ class Client extends EventEmitter {
       }
     }
 
-    if (this.binary && !query.binary) {
+    const binary = this.connectionParameters.binary || defaults.binary
+    if (binary && !query.binary) {
       query.binary = true
     }
 
