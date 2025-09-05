5210a0ef75c5154b18062f10d1497e3cab34e4c9
Prashant Badiger <pbadiger@opentext.com>
2025-09-05 07:13:32 +0000

    OID 20 is for int8 (bigint), and its default parser in 'text' format returns the value as a string, preserving precision

--- packages/vertica-nodejs/lib/type-overrides.js
+++ packages/vertica-nodejs/lib/type-overrides.js
@@ -17,10 +17,10 @@
 const { VerticaType } = require('v-protocol')
 var types = require('pg-types')
 
-// this is a 'temporary' solution allowing us to continue to use pg-types as long as we can to avoid another large 
+// this is a 'temporary' solution allowing us to continue to use pg-types as long as we can to avoid another large
 // package implementation while we get close to the 1.0 release
 types.setTypeParser(VerticaType.Boolean, types.getTypeParser(16, 'text'))
-types.setTypeParser(VerticaType.Integer, types.getTypeParser(21, 'text'))
+types.setTypeParser(VerticaType.Integer, types.getTypeParser(20, 'text'))
 types.setTypeParser(VerticaType.Float, types.getTypeParser(700, 'text'))
 types.setTypeParser(VerticaType.Numeric, types.getTypeParser(9, 'text'))
 types.setTypeParser(VerticaType.Varbinary, types.getTypeParser(9, 'text'))
